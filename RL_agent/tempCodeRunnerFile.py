c912cdd1599717af0a86dc07374aad00130d0b23
Kavishka Lakshan <kavishkalakshan527@gmail.com>
2026-01-27 18:46:26 +0530

    Add common solar forecast logic and update plot images

--- RL_agent/tempCodeRunnerFile.py
+++ RL_agent/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-self.node_battery_power_kw = self.node_battery_power_kw.copy()
+# 1. Common Solar Forecast (1 Value)
+        if self.current_step < self.max_steps:
+            # Use raw weather data from Node 0 as the 'signal'
+            common_solar = np.array([self.solar_episode[self.current_step][0]])
+            load_step = self.load_episode[self.current_step]
+        else:
+            # Safety: Return zeros if episode has ended
+            common_solar = np.array([0.0])
+            load_step = np.zeros(10)
+        
